75a57d4d7b625f70fc9873b3c419d7d6f105f7a3
tylerggf <162388664+tylerggf@users.noreply.github.com>
2026-06-14 23:36:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,11 @@ const path = require("path");
 
 const app = express();
 
-// serve everything in the repo
 app.use(express.static(__dirname));
 
-// force homepage to your file
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "Game.html"));
+  res.sendFile(path.join(__dirname, "biology_test.html"));
 });
 
-// important for Render
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+app.listen(PORT, () => console.log("Server running on port " + PORT));
